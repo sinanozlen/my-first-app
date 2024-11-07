582ecca480478c7efa8da1ff684ceab007834173
Sinan <Sinan@DESKTOP-8AS41IC>
2024-11-08 00:22:54 +0300

    Angular Entrance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.components';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomeComponent,AboutComponent,ContactComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  helloWorld:string = "Hello, world from TS!";
+  pClassName: string= "blue";
+  clickMe(){
+    alert("Beni Tıkladın")
+  }
+  changeHelloWorldVariable(event:any){
+    this.helloWorld = event.target.value;
+
+    
+
+  }
 }
---
+++ src/app/contact/contact.components.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    template: "<h1 style='color : green'>Contact Page</h1>",   
+    selector: "app-contact"
+})
+export class ContactComponent{
+
+}
