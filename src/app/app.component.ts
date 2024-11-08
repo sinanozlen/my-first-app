bc8c358f9949da332ee74ec8469280200b67bfdc
Sinan <Sinan@DESKTOP-8AS41IC>
2024-11-09 02:26:37 +0300

    todoapp uygulamasi yapildi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import { Component } from '@angular/core';
-
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.components';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent,AboutComponent,ContactComponent,FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [FormsModule],
+  template : 
+  `
+  <h1>Todo App</h1>
+  <div>
+  <label>Work</label>
+  <input [(ngModel)]="work">
+  <button (click)="save()">Save</button>
+  </div>
+  <hr>
+  <div>
+  <ul>
+  @for(data of todos; track data){
+    <li>{{ data }}</li>
+  }
+ 
+  </ul>
+  </div>
+  `
 })
 export class AppComponent {
-  helloWorld:string = "Hello, world from TS!";
-  pClassName: string= "blue";
-  clickMe(){
-    alert("Beni Tıkladın")
-  }
-  changeHelloWorldVariable(event:any){
-    this.helloWorld = event.target.value;
-
-    
-
-  }
+  
+  
+work: string = "";
+todos:string[] = [];
+save(){
+this.todos.push(this.work)
 }
+  }
